255d594b326c577f04b1c3e305ddbd1936010327
Maksim <mx123@yandex.ru>
2019-02-28 21:20:59 +1000

    Выполнен второй пункт задания

--- src/lesson1/ArrayUtills.java
+++ src/lesson1/ArrayUtills.java
@@ -1,9 +1,25 @@
 package lesson1;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class ArrayUtills {
     public static <T> void changePlace(T[] array, int position1, int position2) {
         T obj = array[position1];
         array[position1] = array[position2];
         array[position2] = obj;
+    }
+
+    public static <T> ArrayList<T> getArrayListFromArray(T[] array) {
+        /*ArrayList<T> list = new ArrayList<>();
+        for (T t : array) {
+            list.add(t);
+        }*/
+
+        ArrayList<T> list = new ArrayList<>();
+        Collections.addAll(list, array);
+
+        return list;
     }
 }
--- src/lesson1/Lesson1.java
+++ src/lesson1/Lesson1.java
@@ -1,24 +1,51 @@
 package lesson1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Lesson1 {
     public static void main(String[] args) {
         //1
-        testFirstTask();
+//        testFirstTask();
+        //2
+        testSecondTask();
     }
 
     public static void testFirstTask(){
-        String[] strArray = {"Один", "Два", "Три"};
+        String[] strArray = getStringArray();
         System.out.println(Arrays.toString(strArray));
         ArrayUtills.changePlace(strArray, 1, 2);
         System.out.println(Arrays.toString(strArray));
 
         System.out.println("-----");
 
-        Integer[] intArray = {1, 2, 3};
+        Integer[] intArray = getIntArray();
         System.out.println(Arrays.toString(intArray));
         ArrayUtills.changePlace(intArray, 1, 2);
         System.out.println(Arrays.toString(intArray));
+    }
+
+    public static void testSecondTask(){
+        String[] strArray = getStringArray();
+        System.out.println(Arrays.toString(strArray));
+        ArrayList<String> stringList = ArrayUtills.getArrayListFromArray(strArray);
+        System.out.println(stringList);
+
+        System.out.println("-----");
+
+        Integer[] intArray = getIntArray();
+        System.out.println(Arrays.toString(intArray));
+        ArrayList<Integer> intList = ArrayUtills.getArrayListFromArray(intArray);
+        System.out.println(intList);
+    }
+
+    public static String[] getStringArray(){
+        String[] strArray = {"Один", "Два", "Три"};
+        return strArray;
+    }
+
+    public static Integer[] getIntArray(){
+        Integer[] intArray = {1, 2, 3};
+        return intArray;
     }
 }
